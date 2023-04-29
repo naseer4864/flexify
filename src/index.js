422fb998ccd0a56e340690e9ea2956a14c411d8e
naseer4864 <naseernoor4864@gmail.com>
2023-04-29 08:30:57 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/firebase.context";
+import 'normalize.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
